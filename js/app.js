8e4a9b3343288462c853d0b7ee88de3f470c8c24
Maryi Liliana Bustos Carrillo <maryie098@hotmail.com>
2018-11-21 19:02:18 -0500

    Cositas

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/* ELEMENTOS */
+var botonAtras = document.querySelector('#barra-superior>.boton-atras');
+var botonMenu = document.querySelector('#barra-superior>.boton-menu');
+var menuLateral = document.querySelector('#menu-lateral');
+
+
+/* REGISTRAR EVENTOS */
+// Click en boton atras
+botonAtras.addEventListener('click', function(e){
+    volverAtras();
+});
+// Click en boton menu
+botonMenu.addEventListener('click', function(e){
+    abrirMenu();
+});
+
+/* FUNCIONES */
+// Ir atras
+function volverAtras(){
+    window.history.back();
+}
+// Abrir menú
+function abrirMenu(){
+    menuLateral.classList.toggle('menu-abierto');
+}
